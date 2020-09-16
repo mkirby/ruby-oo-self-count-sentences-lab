78e2c86113237c6e28cb7642d740ed80778f5dfc
Matthew Kirby <matt.kirby@yahoo.com>
2020-09-15 22:11:09 -0400

    finished lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    # find those pesky punctuations, convert them to periods,
+    # split the sentences on the last period and space
+    # and count the elements in the array.
+    self.gsub(/\?|\!/,".").split(". ").count
   end
 end
